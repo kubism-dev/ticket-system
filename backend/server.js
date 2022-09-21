3f300fd599f045b74140a56fbe06d384ee0c4f4d
Kjubism <dev@hellothere.de>
2022-09-21 21:49:23 +0200

    add express production environment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv').config();
 const color = require('colors');
 const { errorHandler } = require('../backend/middleware/errorHandler');
@@ -20,6 +21,19 @@ app.get('/', (req, res, next) => {
 // API Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
+} else {
+  app.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+  })
+}
 
 app.use(errorHandler);
 
