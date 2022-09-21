6cf147dbf8e85f8744ae0980e09274f9f578ac3f
Kjubism <dev@hellothere.de>
2022-09-21 21:58:19 +0200

    fix undefined reference

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_DB);
     console.log(`We connectin' ${conn.connection.host}`.cyan.underline);
   } catch (err) {
-    console.log(`Error ${error.message}`.red.underline.bold);
+    console.log(`Error ${err.message}`.red.underline.bold);
     process.exit(1);
   }
 };
