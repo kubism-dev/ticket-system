c01207db7c692acb9d670e115e20f01f3795133b
Kjubism <dev@hellothere.de>
2022-09-21 22:10:10 +0200

    remove temp page

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res, next) => {
-  res.status(200).send('hi');
-});
-
 // API Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
